1f3a3373816ad3e6f76c7e26b9c4e5f5754a5bdc
Josh Chorlton <jchorlton@gmail.com>
2017-07-04 00:26:40 -0400

    barcode scanning works

--- components/BarcodeScanner/index.js
+++ components/BarcodeScanner/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Expo, { BarCodeScanner, Permissions } from 'expo';
 import { connect } from 'react-redux';
+import HmacSHA256 from 'crypto-js/hmac-sha256';
+import Base64 from 'crypto-js/enc-base64';
 import { setBarcodeInfo } from '../../actions';
+import { AWS_KEY_ID, AWS_SECRET } from '../../credentials';
 
 class BarcodeScanner extends React.Component {
     constructor(props) {
@@ -37,8 +40,18 @@ class BarcodeScanner extends React.Component {
 
     _handleBarCodeRead = data => {
         const { dispatch, navigation } = this.props;
+        const host = 'http://webservices.amazon.com/onca/xml';
+        const idType = 'EAN';
+        const itemId = data.data;
+        const queryString = `Service=AWSECommerceService&AWSAccessKeyId=${AWS_KEY_ID}&AssociateTag=joshchorlton-20&Operation=ItemLookup&SearchIndex=All&IdType=${idType}&ItemId=${itemId}&Timestamp=${(new Date()).toISOString()}`.replace(/,/g, '%2C').replace(/:/g, '%3A');
+        const split = queryString.split('&');
+        split.sort();
+        const sortedQueryString = split.join('&');
+        const stringToSign = 'GET\nwebservices.amazon.com\n/onca/xml\n' + sortedQueryString;
+        const signature = Base64.stringify(HmacSHA256(stringToSign, AWS_SECRET)).replace(/\+/g, '%2B').replace(/=/g, '%3D');
+        const url = host + '?' + sortedQueryString + '&Signature=' + signature;
+        console.log(url);
         dispatch(setBarcodeInfo({name: 'Tostitos', amount: 200}));
-        console.log(JSON.stringify(data));
         navigation.goBack();
     }
 }
